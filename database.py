d82f0ef6458d178dd68ff9e2895ef218e285ab2e
Nightmare <datalab2026@gmail.com>
2026-04-04 20:44:16 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,13 @@
+import os
+import config
+from pymongo import MongoClient
 
+# Берем пароль из твоего конфига (где os.environ.get)
+password = config.MONGO_URL
+
+# Собираем строку. Буква f перед кавычками обязательна — она подставляет переменную
+uri = f"mongodb+srv://Database:{password}@cluster0.ni6tpad.mongodb.net/?appName=Cluster0"
+
+# Подключаемся
+client = MongoClient(uri)
+db = client.get_default_database() # Автоматически выберет базу из кластера
